d9e030f292fdcc663aeaec527ce042bea939a482
Mustafa Melik Altuğ <mustafamelik.altug@gmail.com>
2023-06-17 14:30:41 +0300

    Primary Layout Design

--- app/src/main/java/com/example/tvshowsapp/activites/MainActivity.java
+++ app/src/main/java/com/example/tvshowsapp/activites/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.tvshowsapp;
+package com.example.tvshowsapp.activites;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.tvshowsapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
